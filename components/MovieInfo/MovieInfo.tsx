f2d797c3f98baa60e25be29f98f36cc795a3077e
Eugeniya Dzynia <evgeniya.cherevko@gmail.com>
2022-09-23 16:35:32 +0200

    created new section tag

--- components/MovieInfo/MovieInfo.tsx
+++ components/MovieInfo/MovieInfo.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image';
+//Helping functions
+import { calcTime, convertMoney } from '../../helpers';
+//Components
+import Thumb from '../Thumb/Thumb';
+import Tag from '../Tag/Tag';
+
 const MovieInfo = () => <div>MovieInfo</div>;
 
 export default MovieInfo;
---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  text: string;
+  className?: string;
+};
+
+const Tag = ({ text, className }: Props) => (
+  <div
+    className={`bg-cyan-100 text-black text-sm font-bold px-2 py-1 m-2 rounded-full inline-block ${className}`}
+  >
+    {text}
+  </div>
+);
+
+export default Tag;
